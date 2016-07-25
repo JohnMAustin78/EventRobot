6aef4845b8868f85f6bb932645143fbf2edf8c3b
JohnAustin-msft <johnau@microsoft.com>
2016-07-25 13:56:39 -0700

    layout changes-remove floating button, add menu to event activity

--- app/src/main/java/com/microsoft/office/EventRobot/MainActivity.java
+++ app/src/main/java/com/microsoft/office/EventRobot/MainActivity.java
@@ -1,20 +1,10 @@
 package com.microsoft.office.EventRobot;
 
-import android.app.IntentService;
-import android.app.assist.AssistContent;
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,16 +15,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent voiceActivityIntent = new Intent(MainActivity.this, EventActivity.class);
-                MainActivity.this.startActivity(voiceActivityIntent);
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
     }
 
     @Override
