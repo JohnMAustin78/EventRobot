36f7947ca619354d0aec74ae1e190ce46851986c
JohnAustin-msft <johnau@microsoft.com>
2016-07-25 10:33:46 -0700

    added authentication

---
+++ app/src/main/java/com/microsoft/office/EventRobot/Constants.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+package com.microsoft.office.EventRobot;
+
+interface Constants {
+    String AUTHORITY_URL = "https://login.microsoftonline.com/common";
+    // Update these two constants with the values for your application:
+    String CLIENT_ID = "ENTER_YOUR_CLIENT_ID";
+    String REDIRECT_URI = "ENTER_YOUR_REDIRECT_URI";
+    String MICROSOFT_GRAPH_API_ENDPOINT_RESOURCE_ID = "https://graph.microsoft.com/";
+}
